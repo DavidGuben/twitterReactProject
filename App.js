ff3a2f298f6b05e70abc2e493a576240612dc1cc
DavidGuben <davidjg6@gmail.com>
2016-09-29 00:58:45 -0400

    started adding func for online users

--- App.js
+++ App.js
@@ -5,23 +5,77 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor(){
     super();
-    this.state = {txt: "No tweets yet"}
+    this.state = {
+      txt: '',
+      data: [
+        {id: 1, name: "David Guben"},
+        {id: 2, name: "David Frugen"},
+        {id: 3, name: "David Googen"},
+        {id: 4, name: "David Gubert"},
+        {id: 5, name: "David Guren"},
+        {id: 6, name: "David Greel"},
+        {id: 7, name: "David Gubenstein"},
+      ]
+    }
     this.update = this.update.bind(this)
   }
-  // update event
   update(e){
     // this will update the text automatically while you type
     this.setState({txt: e.target.value})
   }
   render(){
+    let rows = this.state.data.map ( person => {
+      return <PersonRow key={person.id} data={person} />
+    })
     return (
-    <div className="container">
+     <div className="container">
       <Header />
       <TweetWidget txt={this.state.txt} update={this.update} />
-    </div>
+      <h3>Online Users</h3>
+      <hr />
+      <table>
+        <tbody>{rows}</tbody>
+      </table>
+     </div>
     );
   }
 }
+// people data
+const PersonRow = (props) => {
+  return (
+    <tr>
+      <td>{props.data.id}</td>
+      <td>{props.data.name}</td>
+    </tr>
+  )
+}
+// Header
+const Header = () => {
+    return (
+      <div className="jumbotron">
+        <h1>Twitter React ES6</h1>
+      </div>
+    )
+}
+// simple stateless component for the glyph icon
+const SearchGlyph = () => <span className="glyphicon glyphicon-ok"></span>
+// stateless tweet widget that passes props
+const TweetWidget = (props) => {
+  return (
+    <div>
+      <h1>{props.txt}</h1>
+      <div className="col-lg-6">
+        <div className="input-group">
+          <input type="text" className="form-control" placeholder="Tweet something..." onChange={props.update} />
+          <span className="input-group-btn">
+          <button className="btn btn-primary" type="button"><SearchGlyph /></button>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
+// TEST COMPONENTS
 // class Counter extends React.Component {
 //   constructor(){
 //     super();
@@ -66,30 +120,16 @@ class App extends React.Component {
 //   }
 // }
 // stateful react button component
-class Header extends React.Component {
-  render(){
-    return (
-      <div className="jumbotron">
-        <h1>Twitter React ES6</h1>
-      </div>
-    )
-  }
-}
-// stateless tweet widget that passes props
-const TweetWidget = (props) => {
-  return (
-    <div>
-      <h1>{props.txt}</h1>
-      <div className="col-lg-6">
-        <div className="input-group">
-          <input type="text" className="form-control" placeholder="Tweet something..." onChange={props.update} />
-          <span className="input-group-btn">
-          <button className="btn btn-default" type="button">Go!</button>
-          </span>
-        </div>
-      </div>
-    </div>
-  );
-}
-
+// class Slider extends React.Component {
+//   render(){
+//     return (
+//       <div>
+//         <input ref="inp" type="range"
+//         min="0"
+//         max="255"
+//         onChange={this.props.update} />
+//       </div>
+//     );
+//   }
+// }
 export default App
